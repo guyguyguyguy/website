20cd37321ff56daacc8e505bc05fd374e5b2463c
Amit Attias <amit@cellogicmobile.com>
2015-04-07 17:00:26 +0300

    fix

--- php/contact-form.php
+++ php/contact-form.php
@@ -11,7 +11,7 @@ header('Expires: ' . gmdate('r', 0));
 header('Content-type: application/json');
 
 // Step 1 - Enter your email address below.
-$to = 'amit@bigabid.com';
+$to = 'tech@bigabid.com';
 
 // Step 2 - Enable if the server requires SMTP authentication. (true/false)
 $enablePHPMailer = true;
